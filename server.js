ec0d99d948b95c769cbe30ca3a695358d145c2a5
ankit59111 <ankit59111>
2018-01-19 20:24:49 +0530

    Initial commit

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ app.use(express.static('__dirname' + '/dist'));
 
 app.listen(process.env.Port || 8080);
 
-
-app.get('/*', function (req, res) => {
- res.sendFile(path.join(__dirname + '/dist/index.html'))
+app.get('/',function (req,res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+
+/*
+app.get('/!*',(req, res) =>
+)
+*/
